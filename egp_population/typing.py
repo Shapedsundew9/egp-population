4b1d56286fa8371f978098ff84b2870ccb59fa21
Shaped Sundew <16618209+Shapedsundew9@users.noreply.github.com>
2023-01-14 17:44:04 -0500

    Latest

--- egp_population/typing.py
+++ egp_population/typing.py
@@ -1,22 +1,40 @@
 """Population Types"""
-from typing import TypedDict
+from typing import TypedDict, Callable, Iterable, Any, NotRequired
 from uuid import UUID
 from datetime import datetime
-from egp_types.ep_type import vtype
+from egp_types.xGC import xGC
 
 
 class Population(TypedDict):
+    uid: NotRequired[int]
+    worker_id: NotRequired[UUID]
+    size: int
+    inputs: tuple[str, ...]
+    outputs: tuple[str, ...]
+    characterize: Callable[[xGC], tuple[float, float]]
+    recharacterize: Callable[[Iterable[xGC]], list[float] | tuple[float, ...]]
+    oih: NotRequired[int]
+    name: str
+    description: NotRequired[str]
+    meta_data: NotRequired[str]
+    created: NotRequired[datetime]
+
+
+class PopulationNorm(TypedDict):
     uid: int
     worker_id: UUID
+    fitness_function_hash: bytes
     size: int
-    inputs: list[str]
-    outputs: list[str]
-    oih: bytes
+    inputs: tuple[str, ...]
+    outputs: tuple[str, ...]
+    characterize: NotRequired[Callable[[xGC], Any]]
+    recharacterize: NotRequired[Callable[[Iterable[xGC]], list[Any] | tuple[Any]]]
+    oih: int
     name: str
     description: str
     meta_data: str
     created: datetime
-    vt: vtype
-    
 
-Populations = dict[int, Population]
+
+Populations = dict[str, Population]
+PopulationsNorm = dict[str, PopulationNorm]
